3ee8ca4088d8fc9af94d26739a36b46ecce7c943
ChunCheng Wei <chunchengwei@weride.ai>
2021-07-31 14:49:21 +0800

    no comment commit

--- update.sh
+++ update.sh
@@ -4,11 +4,11 @@
 # Author: Chuncheng Wei
 # Mail: weicc1989@gmail.com
 # Created Time : Tue 16 Jan 2018 05:37:16 PM DST
-# Last Modified: Mon Sep  2 13:09:13 2019
+# Last Modified: 2021年07月31日 星期六 14时48分46秒
 #******************************************************************************
 
 # input
-msg="temp update"
+msg="no comment commit"
 if [ $# != 0 ]; then
     msg="update: $*"
 fi
